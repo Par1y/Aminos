c2b0c5ce76b7ff56eea9bb01e29739bc33194abb
Par1y <xzfiszoombie@163.com>
2025-11-24 16:08:34 +0800

    requirement update

--- dify_plugin/chromedriver/provider/chromedriver.py
+++ dify_plugin/chromedriver/provider/chromedriver.py
@@ -6,15 +6,18 @@ from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 from selenium import webdriver
 from selenium.webdriver.chromium.options import ChromiumOptions
 
+
 class ChromedriverProvider(ToolProvider):
-    
+
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         driver = None
         try:
             uri = credentials.get("chromedriver_uri")
             if not uri:
-                raise ToolProviderCredentialValidationError("chromedriver_uri is required.")
-            
+                raise ToolProviderCredentialValidationError(
+                    "chromedriver_uri is required."
+                )
+
             # driver = webdriver.Remote(command_executor=uri, options=ChromiumOptions())
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
